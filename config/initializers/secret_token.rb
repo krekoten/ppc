2190dc451a070ada377896d0ecc37b6802771c62
Krekoten Marjan <krekoten@intensol.com>
2010-08-03 23:07:11 +0300

    Створив базову структуру

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppc::Application.config.secret_token = '30833ca4ee4a1e80213f93c02b7365e6e7b9b2473c5bf3d7c6f5bbba9629162a0ec9bb5d06d8f83e0c731c3ec5cbb9fcadb751e3dd1e5070718c54849c1decaa'
